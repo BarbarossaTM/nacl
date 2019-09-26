013360b8cb6c14abe59b8de86c869689f3691085
Maximilian Wilhelm <max@sdn.clinic>
2019-09-26 21:21:16 +0200

    Real life config example for tag matching.
    Signed-off-by: Maximilian Wilhelm <max@sdn.clinic>

--- nacl_config.json.ex
+++ nacl_config.json.ex
@@ -21,4 +21,73 @@
 		}
 	}
 
+	"tags" : {
+		"interface" : {
+			"batman_connect_sites" : {
+				"match" : {
+					"re": "^batman_connect_(.*)$"
+					"params" : 1,
+				},
+				"set" : {
+					"field" : "batman_connect_sites",
+					"type" : "list"
+				}
+			},
+
+			"batman_iface" : {
+				"match" : {
+					"re" : "^batman_iface_(.*)$",
+					"params" : 1
+				},
+				"set" : [
+					{
+						"field" : "type",
+						"value" : "batman_iface"
+					},
+					{
+						"field" : "site"
+					}
+				]
+			},
+
+			"mesh_breakout" : {
+				"match" : {
+					"re" : "^mesh_breakout_(.*)$",
+					"params" : 1
+				},
+				"set" : [
+					{
+						"field" : "type",
+						"value" : "mesh_breakout"
+					},
+					{
+						"field" : "site"
+					}
+				]
+			}
+
+
+			"dhcp" : {
+				"match" : {
+					"name": "dhcp"
+				},
+				"set" : {
+					"field" : "method",
+					"value" : "dhcp"
+				}
+			},
+
+			"status" : {
+				"match" : {
+					"name" : "plannend"
+				},
+				"set" : {
+					"field" : "status",
+					"value" : "planned",
+					"default" : "active"
+				}
+			}
+		}
+	}
+
 }
