9fbf773754bef047f772089dd2119af13a82bd4b
Maximilian Wilhelm <max@sdn.clinic>
2024-05-01 19:11:42 +0200

    salt: Rework ext_pillar, store config in top key
      Reworked the module slightly to improve error handling and also store
      the resulting configuration generated for the given minion_id in the
      top-level pillar key 'node_config'.  This is done in preparation for
      deprecating the 'nodes' key in the longer term future and thereby to
      reduce the pillar size and remove more logic from Salt and Jinja
      templates.
    
    Signed-off-by: Maximilian Wilhelm <max@sdn.clinic>

--- salt/ext_pillar/nacl.py
+++ salt/ext_pillar/nacl.py
@@ -5,41 +5,48 @@
 #
 # -*- coding: utf-8 -*-
 #
-
-import json
 import requests
 
+def ext_pillar(minion_id, pillar, *args, **kwargs):
+    data = {
+        'nodes': {}
+    }
 
-def ext_pillar (minion_id, pillar, *args, **kwargs):
-	data = {
-		'nodes' : {}
-	}
+    try:
+        nodes = _query(f"http://localhost:5000/salt/get_pillar_info?minion_id={minion_id}")
+    except Exception as e:
+        raise Exception(f"query NACL: {str(e)}")
 
-	nodes = _query ("http://localhost:5000/salt/get_pillar_info?minion_id=%s" % minion_id)
+    # Store configuration of the minion we asked for in 'node_config' pillar key.
+    try:
+        pillar['node_config'] = nodes[minion_id]
+    except KeyError as e:
+        raise Exception(f"No NACL result for minion {minion_id}")
 
-	# If there are no nodes defined in pillar at all, just use ours and be done with it
-	if not 'nodes' in pillar:
-		data['nodes'] = nodes
-		return data
+    # Store all node's configuration in 'nodes' pillar key for compatibility
 
-	# IF there are nodes defined in pillar, they take precedence, so only add nodes from
-	# NACL which aren't in pillar
-	pillar_nodes = pillar.get ('nodes', {})
-	for node in nodes:
-		if node in pillar_nodes:
-			continue
+    # If there are no nodes defined in pillar at all, just use ours and be done with it
+    if not 'nodes' in pillar:
+        data['nodes'] = nodes
+        return data
 
-		data['nodes'][node] = nodes[node]
+    # IF there are nodes defined in pillar, they take precedence, so only add nodes from
+    # NACL which aren't in pillar
+    pillar_nodes = pillar.get('nodes', {})
+    for node in nodes:
+        if node in pillar_nodes:
+            continue
 
-	return data
+        data['nodes'][node] = nodes[node]
+
+    return data
 
 
-def _query (url):
-	res = requests.get (url)
-	if res.status_code != 200:
-		raise Exception ("Got a %s from NACL: %s" % (res.status_code, res.text))
+def _query(url):
+    res = requests.get(url)
+    res.raise_for_status()
 
-	try:
-		return res.json ()
-	except Exception as e:
-		raise Exception ("Failed to deserialize NACL data: %s" % str (e))
+    try:
+        return res.json()
+    except Exception as e:
+        raise Exception(f"deserialize JSON: {str (e)}")
